0422cc158c86bf79558366c9d8ab3a719d55ae84
unknown <jamalintalo@gmail.com>
2025-10-31 03:56:01 +0300

    Added main method and schoolManagement classes with variables

--- src/school/management/Main.java
+++ src/school/management/Main.java
@@ -1,2 +1,15 @@
-package school.management;
+// Implementation by Jamal Intalo
+//This is the main method
 
+package school.management;
+ class Main{
+    public static void main(String[] args){
+        SchoolManagement mySchool= new SchoolManagement();
+        mySchool.setSchoolName("Islamic Uni");
+        mySchool.setAddress("40441 Abubakr as Siddiq road");
+        mySchool.setContactNumber("0595318390");
+        mySchool.setMediumOfStudy("English/ Arabic");
+
+        System.out.println("School name: "+mySchool.getSchoolName());
+    }
+ }
--- src/school/management/SchoolManagement.java
+++ src/school/management/SchoolManagement.java
@@ -1,5 +1,46 @@
+//Implemantation by Jamal Intalo
+//This is the schoolManagement class
 package school.management;
 
-public class SchoolManagement {
-    
+public class SchoolManagement{
+    private String schoolName;
+    private String address;
+    private String contactNumber;
+    private String mediumOfStudy;
+ 
+
+  //GETTER METHODS FOR VARIABLES
+
+public String getSchoolName(){
+    return schoolName;
+}
+
+public String getAddress(){
+    return address;
+}
+
+public String getMediumOfStudy(){
+    return mediumOfStudy;
+}
+
+public String getContactNumber(){
+    return contactNumber;
+}
+
+//SETTER METHODS
+public void setSchoolName(String name){
+    this.schoolName=name;
+}
+
+public void setAddress(String address){
+    this.address=address;
+}
+
+public void setContactNumber(String contact){
+    this.contactNumber=contact;
+}
+
+public void setMediumOfStudy(String medium){
+    this.mediumOfStudy=medium;
+ }
 }
