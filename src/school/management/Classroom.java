f26e455ef2de9a2fd47a2dd0d24ffb0430b7f5c9
unknown <jamalintalo@gmail.com>
2025-10-31 06:23:00 +0300

    Attributes & Methods of classroom added

--- src/school/management/Classroom.java
+++ src/school/management/Classroom.java
@@ -1,5 +1,54 @@
+// Implemantation by Jamal Intalo
+// This is the classroom class
+
 package school.management;
 
 public class Classroom {
+    private int classId;
+    private String className;
+    private String teacherName;
+    private int studentCount;
+    private int equipmentCount;
     
+//GETTERS FOR VARIABLES
+    public int getClassId(){
+        return classId;
+    }
+    public String getClassName(){
+        return className;
+    }
+    public String getTeacherName(){
+        return teacherName;
+    }
+    public int getStudentCount(){
+        return studentCount;
+    }
+    public int getEquipmentCount(){
+        return equipmentCount;
+    }
+//SETTERS FOR VARIABLES
+    public void setClassId(int classId){
+        this.classId=classId;
+    }
+    public void setClassName(String className){
+        this.className=className;
+    }
+    public void setTeacherName(String teacherName){
+        this.teacherName=teacherName;
+    }
+    public void setStudentCount(int studentCount){
+        this.studentCount=studentCount;
+    }
+    public void setEquipmentCount(int equipmentCount){
+        this.equipmentCount=equipmentCount;
+    }
+//METHOD FOR CLASSROOM CLASS
+  //ClassDetails Method
+    void display(){
+        System.out.println("Class ID: "+getClassId());
+        System.out.println("Class Name: "+ getClassName());
+        System.out.println("Teacher Name: "+ getTeacherName());
+        System.out.println("Student Count: "+ getStudentCount());
+        System.out.println("Equipment Count: "+ getEquipmentCount());
+    }
 }
--- src/school/management/Main.java
+++ src/school/management/Main.java
@@ -9,7 +9,18 @@ package school.management;
         mySchool.setAddress("40441 Abubakr as Siddiq road");
         mySchool.setContactNumber("0595318390");
         mySchool.setMediumOfStudy("English/ Arabic");
+        mySchool.display();
 
-        System.out.println("School name: "+mySchool.getSchoolName());
+        System.out.println( );
+
+        Classroom class5=new Classroom();
+        class5.setClassId(1);
+        class5.setClassName("West");
+        class5.setTeacherName("Ali");
+        class5.setStudentCount(40);
+        class5.setEquipmentCount(40);
+        class5.display();
+
+
     }
  }
